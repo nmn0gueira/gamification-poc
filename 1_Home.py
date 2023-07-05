22fdeb614d3d10581978fd3899729504f513931e
Nuno <nm.nogueira@campus.fct.unl.pt>
2023-07-05 11:45:58 +0100

    Initial commit

---
+++ 1_Home.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from datagen import generate_dataset
+# Import other necessary modules/functions
+
+def run_app():
+    st.sidebar.title("Employee Performance Dataset Generator")
+    number_of_employees = st.sidebar.number_input("Number of Employees", min_value=1, value=100)
+    dataset_name = st.sidebar.text_input("Dataset Name", "Dataset 1")  # User-defined dataset name
+
+    if st.sidebar.button("Generate Dataset"):
+        dataset = generate_dataset(dataset_name, number_of_employees)
+        st.session_state.datasets[dataset_name] = dataset
+        st.session_state.selected_dataset = dataset_name  # Update the selected dataset
+
+    if not st.session_state.datasets:
+        st.info("No datasets available. Please generate a dataset.")
+    else:
+        dataset_names = list(st.session_state.datasets.keys())
+        dataset_to_display = st.selectbox("Select Dataset", dataset_names, index=dataset_names.index(st.session_state.selected_dataset),
+                                          key="dataset_selectbox")
+        st.session_state.selected_dataset = dataset_to_display  # Update the selected dataset
+        st.dataframe(st.session_state.datasets[st.session_state.selected_dataset])
+
+
+if __name__ == "__main__":
+    if "datasets" not in st.session_state:
+        st.session_state.datasets = {}  # Create the datasets dictionary in session_state
+
+    if "selected_dataset" not in st.session_state:
+        st.session_state.selected_dataset = None  # Initialize the selected dataset
+
+    run_app()
---
+++ datagen.py
@@ -0,0 +1,88 @@
+import random
+import pandas as pd
+import datetime
+import holidays
+import streamlit as st
+
+
+MIN_UNITS_PROCESSED = 100
+MAX_UNITS_PROCESSED = 300
+
+PERCENTAGE_OF_ALLOWED_DEFECTS = 0.1
+
+EFFICIENCY_FACTOR_MIN = 0.8
+EFFICIENCY_FACTOR_MAX = 1.0
+
+def get_business_days(leave_days=None):
+    if leave_days is None:
+        leave_days = []
+    now = datetime.datetime.now()
+    pt_holidays = holidays.country_holidays('PT')
+    business_days = 0
+    for i in range(1, 32):
+        try:
+            this_date = datetime.date(now.year, now.month, i)
+        except ValueError:
+            break
+        # Monday == 0, Sunday == 6
+        if this_date.weekday() < 5 and this_date not in pt_holidays and this_date.day not in leave_days:
+            business_days += 1
+
+    return business_days
+
+
+# units per hour
+def get_productivity(units_processed, number_of_defects, total_labor_hours, efficiency_factor):
+    return (units_processed - number_of_defects) / (total_labor_hours * efficiency_factor)
+
+# time per unit (hours)
+def get_unit_processing_time(units_processed, number_of_defects, total_labor_hours, efficiency_factor):
+    return (total_labor_hours * efficiency_factor) / (units_processed - number_of_defects)
+
+def generate_dataset(number_of_employees):
+
+    data = {'employee_id': [],
+            'units_processed': [],
+            'number_of_defects': [],
+            'total_labor_hours': [],
+            'efficiency_factor': [],
+            # 'quality_factor': [],    # for debug purposes
+            'unit_processing_time': [],
+            'productivity': []      # for debug purposes
+            }
+
+    for i in range(number_of_employees):
+        # Variables
+        employee_id = i + 1
+
+        units_processed = random.randint(MIN_UNITS_PROCESSED, MAX_UNITS_PROCESSED)
+
+        max_number_of_defects = int(units_processed * PERCENTAGE_OF_ALLOWED_DEFECTS)    # Round down number
+        number_of_defects = random.randint(0, max_number_of_defects)
+        
+        leave_days = random.sample(range(1, 32), random.randint(0, 5))
+        total_labor_hours = get_business_days(leave_days) * 8
+
+        efficiency_factor = random.uniform(EFFICIENCY_FACTOR_MIN, EFFICIENCY_FACTOR_MAX)
+
+        # quality_factor = (units_processed - number_of_defects) / units_processed
+
+
+        unit_processing_time = get_unit_processing_time(units_processed, number_of_defects, total_labor_hours, efficiency_factor)
+
+        productivity = get_productivity(units_processed, number_of_defects, total_labor_hours, efficiency_factor)
+        
+        # Add to dataframe
+        data['employee_id'].append(employee_id)
+        data['units_processed'].append(units_processed)
+        data['number_of_defects'].append(number_of_defects)
+        data['total_labor_hours'].append(total_labor_hours)
+        data['efficiency_factor'].append(efficiency_factor)
+        # data['quality_factor'].append(quality_factor)
+        data['unit_processing_time'].append(unit_processing_time)
+        data['productivity'].append(productivity)
+
+    # pd.DataFrame(data).to_csv(csv_name, index=False)
+    print("Dataset generated successfully!")
+
+    return pd.DataFrame(data)
