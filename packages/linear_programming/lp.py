4650d6a77aa8962f5a81d828dcc8eefa7ebc9cb7
Nuno <nm.nogueira@campus.fct.unl.pt>
2023-07-07 15:25:45 +0100

    Started integration of linear prog. in streamlit

---
+++ packages/linear_programming/lp.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from pulp import *
+
+MIN_HOURS_WORKED = 100
+
+
+def solve_linear_programming(dataframe: pd.DataFrame):
+
+    variables = {}
+    number_of_employees = len(dataframe.columns) - 1
+
+    # Create model
+    model = LpProblem("Maximize_Production", LpMaximize)
+
+    
+    # Create variables
+    for key in variables:   # For each task
+        for i in range(number_of_employees):    # For each employee
+            variables[key].append(LpVariable("X" + key + str(i+1), lowBound=0, cat="Integer"))   # Create variable
+
+
+    # Define objective
+    model += lpSum(variables.values()) # For example: (Xprinting1 + Xprinting2 + Xprinting3) + (Xcutting1 + Xcutting2 + Xcutting3) + ...
+    
+
+    # Add constraints
+    # Related to the number of tasks executed being equal between machines
+    values = list(variables.values())
+    for i in range(1, len(values)):    # For each task
+        model += lpSum(values[i]) == lpSum(values[0]) # Sum of each task's variables list must be equal to each other
+
+
+    # Related to the unit processing times from the dataframe
+    for key in variables:   # For each task
+        aux_list = []
+
+        for i in range(number_of_employees):    # For each employee
+            aux_list.append(variables[key][i]*dataframe.loc[key][i])
+
+        model += lpSum(aux_list) <= dataframe.loc[key]['Capacity'] # Sum of each task's variables list must be less than or equal to the task's capacity
+
+
+    # Related to the employees' capacities
+    for i in range(number_of_employees):    # For each employee
+        aux_list = []
+
+        for key in variables:   # For each task
+            aux_list.append(variables[key][i]*dataframe.loc[key][i])
+        
+        model += lpSum(aux_list) >= MIN_HOURS_WORKED # In total, the time spent by an employee must be greater than or equal to the minimum hours worked
+
+    # Solve model
+    model.solve()
+
+
+    # Show solution
+    for v in model.variables():
+        print(v.name, "=", v.varValue)
+
+    return {v.name: v.varValue for v in model.variables()}  # Variables and their values
--- pages/3_Linear_Programming.py
+++ pages/3_Linear_Programming.py
@@ -3,6 +3,35 @@ import streamlit as st
 def run_app():
     st.title("Linear Programming")
 
+    number_of_employees = len(st.session_state.datasets[st.session_state.selected_dataset]) # Each employee is a row in the dataset
+
+    dataframe = pd.DataFrame(columns=["Person " + str(i+1) for i in range(number_of_employees)])
+    capacities = []
+    variables = {}
+
+    # Create complete dataframe with unit processing times
+    # Load from datasets
+    for task_name, dataset in st.session_state.datasets.items():
+        capacity = int(input("Enter capacity: "))
+
+        unit_processing_times = dataset['unit_processing_time'].values
+        dataframe.loc[task_name] = unit_processing_times
+
+        capacities.append(capacity)
+        variables[task_name] = []
+    
+    dataframe['Capacity'] = capacities
+
+
+
+  
+if __name__ == '__main__':
+    import sys
+    import subprocess
+    import pandas as pd
+    from pulp import *
+
+
 if __name__ == "__main__":
     st.set_page_config(page_title="Linear Programming", page_icon=":books:", layout="wide")
 
