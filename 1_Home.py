12ca3e90f24a6f094b0e7e8c366afb343ee6946c
Nuno <nm.nogueira@campus.fct.unl.pt>
2023-07-18 17:01:01 +0100

    Missing gamification in the home page

--- 1_Home.py
+++ 1_Home.py
@@ -18,112 +18,147 @@ def run_app():
                  - solve linear programming models to see how to better organize employees between various tasks. 
                  - gamify the process by assigning points to each task and employee, and then displaying the results in leaderboards.""")
         
-        st.write("Get started by exploring the options below.")
+        st.write("Get started by exploring how the app works.")
 
 
-    # ---- WHAT I DO ----
+    # ---- DATASET GENERATOR ----
     with st.container():
         st.write("---")
-        left_column, right_column = st.columns(2)
-        with left_column:
-            st.header("What I do")
-            st.write("##")
-            st.write(
-                """
-                On my YouTube channel I am creating tutorials for people who:
-                - are looking for a way to leverage the power of Python in their day-to-day work.
-                - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-                - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-                - are working with Excel and found themselves thinking - "there has to be a better way."
-
-                If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
-                """
-            )
-            st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
-        with right_column:
-            st_lottie(lottie_coding, height=300, key="coding")
-
-    # ---- PROJECTS ----
-    with st.container():
-        st.write("---")
-        st.header("My Projects")
-        st.write("##")
-        image_column, text_column = st.columns((1, 2))
-        with image_column:
-            st.empty()
-        with text_column:
-            st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
-            st.write(
-                """
-                Learn how to use Lottie Files in Streamlit!
-                Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
-                In this tutorial, I'll show you exactly how to do it
-                """
-            )
-            st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
-    with st.container():
-        image_column, text_column = st.columns((1, 2))
-        with image_column:
-            st.empty()
-        with text_column:
-            st.subheader("How To Add A Contact Form To Your Streamlit App")
-            st.write(
-                """
-                Want to add a contact form to your Streamlit website?
-                In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
-                """
-            )
-            st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
-
-    # ---- CONTACT ----
-    with st.container():
-        st.write("---")
-        st.header("Get In Touch With Me!")
-        st.write("##")
-
-        # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-        contact_form = """
-        <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
-            <input type="hidden" name="_captcha" value="false">
-            <input type="text" name="name" placeholder="Your name" required>
-            <input type="email" name="email" placeholder="Your email" required>
-            <textarea name="message" placeholder="Your message here" required></textarea>
-            <button type="submit">Send</button>
-        </form>
-        """
-        left_column, right_column = st.columns(2)
-        with left_column:
-            st.markdown(contact_form, unsafe_allow_html=True)
-        with right_column:
-            st.empty()
-
-    st.markdown("##")
-
-    st.markdown("### What is Productivity?")
-
-    st.markdown("Productivity describes various measures of the efficiency of production. A productivity measure is expressed as the ratio of output to inputs used in a production process, i.e. output per unit of input. Productivity is a crucial factor in production performance of firms and nations. Increasing national productivity can raise living standards because more real income improves people's ability to purchase goods and services, enjoy leisure, improve housing and education and contribute to social and environmental programs. Productivity growth also helps businesses to be more profitable.")
-
-
-
-
-
-    st.header("Gamification")
-
-    st.markdown("##")
-
-    st.markdown("### What is Gamification?")
-    st.markdown("Gamification is the application of game-design elements and game principles in non-game contexts. It can also be defined as a set of activities and processes to solve problems by using or applying the characteristics of game elements. Gamification commonly employs game design elements to improve user engagement, organizational productivity, flow, learning, crowdsourcing, employee recruitment and evaluation, ease of use, usefulness of systems, physical exercise, traffic violations, voter apathy, and more. A collection of research on gamification shows that a majority of studies on gamification find it has positive effects on individuals. However, individual and contextual differences exist.")
-    st.markdown("### Why Gamification?")
-    st.markdown("Gamification is a powerful tool to engage people. It is a way to motivate people to change their behavior, develop skills and drive innovation. It is a way to inspire people to do things they otherwise would not do, such as completing boring or difficult tasks, or learning new things. Gamification is a way to make people feel good about themselves and their accomplishments. It is a way to make people feel like they are part of something bigger than themselves. It is a way to make people feel like they are part of a community. It is a way to make people feel like they are part of a team. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause. It is a way to make people feel like they are part of a movement. It is a way to make people feel like they are part of a cause.")
-
-
-    with st.sidebar:
-        st.header("About the project")
-        st.write("This project was created by [Nuno Nogueira] during an internship at [INOV INESC](https://www.inov.pt/sobre-nos/index.html) as part of FCT/UNL Internship Program.")
         
+        st.header("Dataset Generator")
+        st.markdown("##")
+        st.write(
+            """
+            This component allows you to create, manage and delete datasets for workable employee data.
+            Each dataset is meant to represent monthly data of a certain task of the workers, and it contains the following information:
+            - Employee ID
+            - Units processed
+            - Hours worked
+            - Efficiency factor (factors in time spent on breaks, lunch, etc.)
+            - Unit processing time (time it takes to process one unit)
+            """
+        )
+
+        st.markdown("##")
+
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write(
+                """
+                On the sidebar, you will find input elements to configure the dataset generation process:
+                - "Number of Employees": The desired number of employees for the dataset. The minimum allowed value is 5.
+                - "Capacity (hrs)": The desired capacity for the dataset. Represents the total number of hours available for the task.
+                - "Dataset Name": Name for your dataset. Since each dataset is meant represents a task, it is recommended to use the name of the task as the dataset name.
+                - "Task Difficulty": Use the slider to set the task difficulty, which ranges from 1 to 10, with 1 being the easiest and 10 being the hardest, this will affect the point system in the Gamification section.
+                """
+            )
+            st.write("""After setting the parameters, click the "Generate Dataset" button. The app will create a new dataset based on the provided settings.""")
+            
+            
+        with right_column:
+            # Put other lottie file here
+            st_lottie(lottie_coding, speed=1, height=300, key="lottie_coding")
+            
+
+    # ---- LINEAR PROGRAMMING ----
+    with st.container():
         st.markdown("---")
-        st.header("Source Code")
-        st.write("The source code is available on [GitHub]")
+        st.header("Linear Programming")
+
+        st.info("Before you can solve an LP model, you need to have generated datasets")
+
+        st.markdown("##")
+
+        st.write(
+            """
+            This component allows you to solve linear programming models based on the unit processing times of the generated datasets.
+            """
+        )
+
+        left_column, right_column = st.columns(2)
+
+        with left_column:
+            st.subheader("Build dataframe")
+            st.write(
+                """
+                To solve an LP model, you need to build the dataframe that will be used to solve the model by clicking the "Build Dataframe" button.
+                This dataframe displays the unit processing times of each employee for a given task and the capacity of said task.
+
+                If you generate more datasets after building the dataframe, you will need to rebuild it to include the new datasets.
+                """
+            )
+
+        with right_column:
+            st.subheader("Solve LP model")
+
+            st.write(
+                """
+                After building the dataframe, you can solve the LP model by clicking the "Solve LP Model" button.
+                The input of minimum and maximum hours worked will be used for some of the constraints of the model.
+                
+                The app will display (if the model is solvable) the total number of units produced and the total number of hours worked by the employees as well the information of the model, including the objective function, variables and constraints.
+                """
+            )
+
+        st.markdown("##")
+        
+        st.subheader("About the model info")
+
+        # Model status
+        st.write(
+            """
+            **Model status**: When the model is solvable, the status will be "Optimal" with a success message and the rest of the model info will be displayed. If the model is not solvable, its status will be displayed on screen with an error message.
+            """
+        )
+
+        # Objective function
+        st.write(
+            """
+            **Objective function**: The objective function is always to maximize the sum of all variables.
+            """
+        )
+
+        # Variables
+        st.write(
+            """
+            **Variables**: A variable represents the number of times a task was done by a certain employee. For example, the variable "Xprinting1" indicates the amount of times the task "printing" was done by the employee with ID 1.
+            """
+        )
+
+        # Constraints
+        st.write(
+            """
+            **Constraints**: The constraints are the rules that the model must follow. The constraints of this model are:
+            - Equality constraints: A piece is produced only when all tasks for it are completed. There is no need to do aditional tasks for a piece that cannot be completed.
+            - Unit processing time constraints: The total number of hours worked on a task cannot exceed its capacity.
+            - Minimum hours worked constraints: The total number of hours worked by an employee must be equal to or greater than the minimum hours of work required.
+            - Maximum hours worked constraints: The total number of hours worked by an employee must be equal to or lower than the maximum hours of work allowed.
+            """
+        )
+
+    # ---- GAMIFICATION ----
+    with st.container():
+        st.markdown("---")
+        st.header("Gamification")
+        st.info ("Before you can use the gamification component, you need to have a solved LP model")
+        st.markdown("##")
+
+        st.write(
+            """
+            This component allows you to create leaderboards with automatic point assignment based on how well the employees performed their tasks.
+            """
+        )
+
+
+
+
+    # ---- ABOUT ----
+    with st.sidebar:
+        for _ in range(8):
+            st.markdown("##")
+        st.markdown("---")
+        st.header("About the project")
+        st.write("This project was created by Nuno Nogueira during an internship at [INOV INESC](https://www.inov.pt/sobre-nos/index.html) as part of FCT/UNL Internship Program.")
 
 
 
