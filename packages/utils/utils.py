6463c7428558ee0c6f31338de86798ffeb3ede12
Nuno <nm.nogueira@campus.fct.unl.pt>
2023-07-19 09:28:17 +0100

    Removed local_css function from utils.py

--- packages/utils/utils.py
+++ packages/utils/utils.py
@@ -32,10 +32,6 @@ def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 def hide_streamlit_style():
     hide_streamlit_style = """
